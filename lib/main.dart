34ef48dcd07a36c9b1379044bbfa6e5c920735df
Adelson Júnior <junioradelsonst@gmail.com>
2023-06-05 03:07:33 +0000

    tela de cadastro

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+<<<<<<< HEAD
 import 'package:app_biblioteca/tela_inicial.dart';
+=======
+import 'package:app_biblioteca/tela_cadastro.dart';
+>>>>>>> 731b1bd (tela de cadastro)
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
+<<<<<<< HEAD
       home: TelaInicial(),
+=======
+      home: TelaCadastro(),
+>>>>>>> 731b1bd (tela de cadastro)
     )
   );
 }
---
+++ lib/tela_cadastro.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+
+class TelaCadastro extends StatefulWidget {
+  const TelaCadastro({Key? key}) : super(key: key);
+
+  @override
+  State<TelaCadastro> createState() => _TelaCadastroState();
+}
+
+class _TelaCadastroState extends State<TelaCadastro> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor: Colors.purple,
+          appBar: buildAppBar(),
+          body: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.account_circle,
+                  size: 96.0,
+                  color: Colors.white,
+                ),
+
+                const SizedBox(height: 16.0),
+
+                TextField(
+                  controller: _nameController,
+                  decoration: InputDecoration(
+                    hintText: 'Digite como deseja ser chamado',
+                    labelText: 'Nome',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                  ),
+                ),
+
+                const SizedBox(height: 16.0),
+
+                TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    hintText: 'Digite seu e-mail',
+                    labelText: 'E-mail',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0)),
+                    filled: true,
+                    fillColor: Colors.white,
+                  ),
+                ),
+
+                const SizedBox(height: 16.0),
+
+                TextField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    hintText: 'Digite sua senha',
+                    labelText: 'Senha',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0)),
+                    filled: true,
+                    fillColor: Colors.white,
+                  ),
+                  obscureText: true,
+                ),
+
+                const SizedBox(height: 16.0),
+
+                ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.deepPurple),
+                      foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                      padding: MaterialStateProperty.all<EdgeInsets>(
+                        const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
+                      ),
+                      textStyle: MaterialStateProperty.all<TextStyle>(
+                        const TextStyle(fontSize: 20.0),
+                      ),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                    onPressed: () {
+                      String name = _nameController.text;
+                      String email = _emailController.text;
+                      String password = _passwordController.text;
+
+                      print('Nome: $name');
+                      print('Email: $email');
+                      print('Senha: $password');
+
+                      _nameController.clear();
+                      _emailController.clear();
+                      _passwordController.clear();
+                    },
+                    child: const Text("Cadastre-se")
+                ),
+
+                const SizedBox(height: 16.0),
+
+                const Text(
+                  "Já possui uma conta? Faça o Login", 
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.white
+                    ),
+                ),
+
+                const SizedBox(height: 16.0),
+
+                ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.deepPurple),
+                      foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                      padding: MaterialStateProperty.all<EdgeInsets>(
+                        const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
+                      ),
+                      textStyle: MaterialStateProperty.all<TextStyle>(
+                        const TextStyle(fontSize: 16.0),
+                      ),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text("Login")
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+
+  buildAppBar() {
+    return AppBar(
+      centerTitle: true,
+      backgroundColor: Colors.purple,
+      title: const Text("Cadastrar", style: TextStyle(fontSize: 30)),
+    );
+  }
+}
