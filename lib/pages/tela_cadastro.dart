10c6a41989cd6fbaebdc7ee169eeb3c39daf8147
adelsonsljunior <adelsonsljunior>
2023-08-08 11:48:40 -0300

    feat: validacao de campos (login e cadastro)

--- lib/pages/tela_cadastro.dart
+++ lib/pages/tela_cadastro.dart
@@ -13,6 +13,17 @@ class _TelaCadastroState extends State<TelaCadastro> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  bool _isValidEmail(String email) {
+    // Expressão Regular para validar o email
+    // O padrão verifica se o email possui um formato válido
+    // (uma sequência de caracteres seguida de um @, seguida de outra sequência de caracteres, seguida de um ponto e um domínio)
+    final RegExp emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+
+    return emailRegex.hasMatch(email);
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -33,136 +44,170 @@ class _TelaCadastroState extends State<TelaCadastro> {
             appBar: buildAppBar(),
             body: Padding(
               padding: const EdgeInsets.all(16),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Icon(
-                    Icons.account_circle,
-                    size: 96.0,
-                    color: Colors.white,
-                  ),
-                  const SizedBox(height: 16.0),
-                  Flexible(
-                    child: TextField(
-                      controller: _nameController,
-                      decoration: InputDecoration(
-                        hintText: 'Digite como deseja ser chamado',
-                        labelText: 'Nome',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.account_circle,
+                      size: 96.0,
+                      color: Colors.white,
+                    ),
+                    const SizedBox(height: 16.0),
+                    Flexible(
+                      child: TextFormField(
+                        controller: _nameController,
+                        decoration: InputDecoration(
+                          hintText: 'Digite como deseja ser chamado',
+                          labelText: 'Nome',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                          floatingLabelBehavior: FloatingLabelBehavior.never,
                         ),
-                        filled: true,
-                        fillColor: Colors.white,
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'O nome é obrigatório';
+                          }
+                        },
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 16.0),
-                  Flexible(
-                    child: TextField(
-                      controller: _emailController,
-                      decoration: InputDecoration(
-                        hintText: 'Digite seu e-mail',
-                        labelText: 'E-mail',
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8.0)),
-                        filled: true,
-                        fillColor: Colors.white,
+                    const SizedBox(height: 16.0),
+                    Flexible(
+                      child: TextFormField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          hintText: 'Digite seu e-mail',
+                          labelText: 'E-mail',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8.0)),
+                          filled: true,
+                          fillColor: Colors.white,
+                          floatingLabelBehavior: FloatingLabelBehavior.never,
+                        ),
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return "o e-mail é obrigatório";
+                          } else if (!_isValidEmail(value)) {
+                            return 'O e-mail precisa ser válido';
+                          }
+                        },
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 16.0),
-                  Flexible(
-                    child: TextField(
-                      controller: _passwordController,
-                      decoration: InputDecoration(
-                        hintText: 'Digite sua senha',
-                        labelText: 'Senha',
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(8.0)),
-                        filled: true,
-                        fillColor: Colors.white,
+                    const SizedBox(height: 16.0),
+                    Flexible(
+                      child: TextFormField(
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                          hintText: 'Digite sua senha',
+                          labelText: 'Senha',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8.0)),
+                          filled: true,
+                          fillColor: Colors.white,
+                          floatingLabelBehavior: FloatingLabelBehavior.never,
+                        ),
+                        obscureText: true,
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'A senha é obrigatório';
+                          } else if (value.length < 8) {
+                            return 'A senha deve ter, no mínimo, 8 carácteres';
+                          }
+                        },
                       ),
-                      obscureText: true,
                     ),
-                  ),
-                  const SizedBox(height: 16.0),
-                  Flexible(
-                    child: ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all<Color>(
-                              Colors.deepPurple),
-                          foregroundColor:
-                              MaterialStateProperty.all<Color>(Colors.white),
-                          padding: MaterialStateProperty.all<EdgeInsets>(
-                            const EdgeInsets.symmetric(
-                                vertical: 12.0, horizontal: 24.0),
-                          ),
-                          textStyle: MaterialStateProperty.all<TextStyle>(
-                            const TextStyle(fontSize: 14.0),
-                          ),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8.0),
+                    const SizedBox(height: 16.0),
+                    Flexible(
+                      child: ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                Colors.deepPurple),
+                            foregroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white),
+                            padding: MaterialStateProperty.all<EdgeInsets>(
+                              const EdgeInsets.symmetric(
+                                  vertical: 12.0, horizontal: 24.0),
+                            ),
+                            textStyle: MaterialStateProperty.all<TextStyle>(
+                              const TextStyle(fontSize: 14.0),
+                            ),
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
                             ),
                           ),
-                        ),
-                        onPressed: () {
-                          String name = _nameController.text;
-                          String email = _emailController.text;
-                          String password = _passwordController.text;
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              String name = _nameController.text;
+                              String email = _emailController.text;
+                              String password = _passwordController.text;
 
-                          _nameController.clear();
-                          _emailController.clear();
-                          _passwordController.clear();
+                              _nameController.clear();
+                              _emailController.clear();
+                              _passwordController.clear();
 
-                          Navigator.of(context).pushReplacement(
-                            MaterialPageRoute(builder: (context) => TelaLogin()),
-                          );
-                        },
-                        child: const Text(
-                          "Cadastre-se",
-                          style: TextStyle(fontSize: 14.0, color: Colors.white, fontWeight: FontWeight.bold),
+                              Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                    builder: (context) => TelaLogin()),
+                              );
+                            }
+                          },
+                          child: const Text(
+                            "Cadastre-se",
+                            style: TextStyle(
+                                fontSize: 14.0,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
                           )),
-                  ),
-                  const SizedBox(height: 16.0),
-                  const Flexible(
-                    child: Text(
-                      "Já possui uma conta? Faça o Login",
-                      style: TextStyle(fontSize: 14.0, color: Colors.white, fontWeight: FontWeight.bold),
                     ),
-                  ),
-                  const SizedBox(height: 16.0),
-                  Flexible(
-                    child: ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all<Color>(
-                              Colors.deepPurple),
-                          foregroundColor:
-                              MaterialStateProperty.all<Color>(Colors.white),
-                          padding: MaterialStateProperty.all<EdgeInsets>(
-                            const EdgeInsets.symmetric(
-                                vertical: 12.0, horizontal: 24.0),
-                          ),
-                          textStyle: MaterialStateProperty.all<TextStyle>(
-                            const TextStyle(fontSize: 14.0),
-                          ),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8.0),
+                    const SizedBox(height: 16.0),
+                    const Flexible(
+                      child: Text(
+                        "Já possui uma conta? Faça o Login",
+                        style: TextStyle(
+                            fontSize: 14.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(height: 16.0),
+                    Flexible(
+                      child: ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                Colors.deepPurple),
+                            foregroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white),
+                            padding: MaterialStateProperty.all<EdgeInsets>(
+                              const EdgeInsets.symmetric(
+                                  vertical: 12.0, horizontal: 24.0),
+                            ),
+                            textStyle: MaterialStateProperty.all<TextStyle>(
+                              const TextStyle(fontSize: 14.0),
+                            ),
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
                             ),
                           ),
-                        ),
-                        onPressed: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                                builder: (context) => const TelaLogin()),
-                          );
-                        },
-                        child: const Text("Login")),
-                  ),
-                ],
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                  builder: (context) => const TelaLogin()),
+                            );
+                          },
+                          child: const Text("Login")),
+                    ),
+                  ],
+                ),
               ),
             )),
       ),
