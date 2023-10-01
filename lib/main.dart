287c0d29cf6117206cf7f6e75df942433c181a32
Felipe de Lima <bfelipedelima@gmail.com>
2023-09-30 22:00:52 -0300

    Organizando main

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:app_biblioteca/pages/tela_inicial.dart';
-import 'package:app_biblioteca/pages/tela_principal.dart';
 import 'package:flutter/material.dart';
-
-//import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 Future main() async {
 
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TelaPrincipal(),
+      home: TelaInicial(),
     ),
   );
 }
